dce7fe638a7164e3966d9ca2cf845fa66265a245
Manish Chand Barudi <99408291+ManishChand349@users.noreply.github.com>
2023-03-25 17:53:38 +0530

    Add files via upload

---
+++ onclick/script.js
@@ -0,0 +1,6 @@
+const boxElement = document.getElementById("box")
+const text = document.getElementById("clickMe")
+
+boxElement.addEventListener("click", () => {
+text.innerHTML = "Thank you "
+});
