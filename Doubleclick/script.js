9c66c30cf52fe5ad11debb1e0a295fc595b0b1cc
Manish Chand Barudi <99408291+ManishChand349@users.noreply.github.com>
2023-03-25 18:01:34 +0530

    Add files via upload

---
+++ Doubleclick/script.js
@@ -0,0 +1,7 @@
+const boxElement = document.getElementById("box")
+const text = document.getElementById("clickMe")
+
+boxElement.addEventListener("dblclick", () => {
+     alert("Why You click me why") 
+     
+});
