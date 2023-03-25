0486a34d86825bd0582b41eb14a51a09e43426a6
Manish Chand Barudi <99408291+ManishChand349@users.noreply.github.com>
2023-03-25 18:10:37 +0530

    Add files via upload

---
+++ Mouseove/script.js
@@ -0,0 +1,6 @@
+const boxElement = document.getElementById("box")
+const text = document.getElementById("clickMe")
+
+boxElement.addEventListener("mouseover", () => {
+     boxElement.style.background = "red"
+});
